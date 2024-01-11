192890c1514f55c5f5d9ce96713fffc04a580872
Raghu <154458583+CloudWithRaghu@users.noreply.github.com>
2024-01-11 09:49:36 +0530

    Create remove-old-kernel.sh

---
+++ remove-old-kernel.sh
@@ -0,0 +1,8 @@
+sudo apt remove -y linux-azure-6.2-cloud-tools-6.2.0-1018 \
+linux-azure-6.2-headers-6.2.0-1018 \
+linux-azure-6.2-tools-6.2.0-1018 \
+linux-image-unsigned-6.2.0-1018-azure \
+linux-modules-6.2.0-1018-azure \
+linux-modules-extra-6.2.0-1018-azure
+
+sudo reboot
