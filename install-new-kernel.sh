31b8fda84ece02088cccc8599a7d4d3f3b4d32a9
Raghu <154458583+CloudWithRaghu@users.noreply.github.com>
2024-01-11 09:48:58 +0530

    Create install-new-kernel.sh

---
+++ install-new-kernel.sh
@@ -0,0 +1,6 @@
+sudo apt install -y linux-azure-cloud-tools-5.15.0-1050 \
+linux-azure-headers-5.15.0-1050 \
+linux-azure-tools-5.15.0-1050 \
+linux-image-unsigned-5.15.0-1050-azure \
+linux-modules-5.15.0-1050-azure \
+linux-modules-extra-5.15.0-1050-azure
